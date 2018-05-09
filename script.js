5cc657b560c7560d202bbd26d2c00849ebc19d06
Hannah De Los Reyes <htdelosreyes8@gmail.com>
2018-05-09 10:35:38 -0700

    menu click functionality

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const theDate = new Date().getFullYear();
 const footer = document.getElementById('copywrite');
 footer.innerHTML = `Hannah De Los Reyes &copy;${theDate}. All Rights Reserved`;
+
+const menu = document.querySelector('.menu');
+const dropdown = document.querySelector('ul');
+
+menu.addEventListener('click', () => {
+  if (dropdown.style.display !== "block") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  };
+})
